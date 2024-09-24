f6b6fec4723d219ae45d13a5ace24f55e057078e
mallobcd <m2bc2d.pro@gmail.com>
2024-09-24 16:29:42 +0200

    Mise en place des méthodes et attributs privés pour MainPresenter

--- SmartPit.py
+++ SmartPit.py
@@ -1,12 +1,4 @@
-import asyncio
-import os
-import sys
-import threading
-import re
-import pygame
-from dotenv import get_key
 from AudioAI import AudioAI
-from TextAI import TextAI
 from IRacing import IRacing
 from MainPresenter import MainPresenter
 from TextAI import TextAI
@@ -17,4 +9,4 @@ if __name__ == "__main__":
     audio_AI = AudioAI()
     text_AI = TextAI()
     ui = UI()
-    presenter = MainPresenter(ui, audio_AI, text_AI, ir)
+    MainPresenter(ui, audio_AI, text_AI, ir)
